d1d28c78b4fd12f92cc6731a3d897f428a1ffed4
captainvlad <zadorozhnyy@ucu.edu.ua>
2019-10-14 15:03:27 +0300

    Documentation added

--- file_1.c
+++ file_1.c
@@ -5,46 +5,49 @@
 #include <stdlib.h>
 
 typedef struct {
+	/* something like class */
 	int current_size;
 	int current_max_size;
 	char *ptr;
 } my_string_t;
 
 void change_max_size(my_string_t* arg){
+	/* increases current max size of string */
 	(*arg).current_max_size *= 2;
 }
 
 void change_current_size(my_string_t* arg){
+	/* counts amount of symbols in the string */
 	(*arg).current_size += 1;
 }
 
-char* create_memory(my_string_t* arg){
+char* create_memory(int arg){
+	/* creates memory for string and returns pointer on its first element */
 	char *ptr;
-	ptr = malloc(((*arg).current_max_size + 1) * sizeof (*ptr));
+	ptr = malloc((arg + 1) * sizeof (*ptr));
 	return ptr;
 }
 
 void write_in_string(my_string_t* arg_1, char arg_2[]){
+	/* writes in string */
 	printf("Not finished yet");
 }
 
 my_string_t create_string(){
+	/* function to create string  */
 	my_string_t string;
 	string.current_size = 1;
 	string.current_max_size = 39;
 	my_string_t * pstring = &string;
-	string.ptr = create_memory(pstring);
+	string.ptr = create_memory(string.current_max_size);
 	return string;
 }
 
 int main(){
+	/* testing func */
 	my_string_t A = create_string();
-	printf("%i", A.current_max_size);
-
-
-//	my_string_t * pVladOS = &VladOS;
-//	change_size(pVladOS);
-//	printf("%i", VladOS.current_size);
+	*(A.ptr + 1) = 'A';
+	free(A.ptr + 1);
 	return 0;
 }
 
